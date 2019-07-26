036ef13ae81d10fb51fe59e5bbda32f714ab4584
equal-l2 <eng.equall2@gmail.com>
2019-07-26 22:32:53 +0900

    arping kan-sei

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@
 
 #include <array>
 #include <vector>
+#include <optional>
+#include <chrono>
+#include <thread>
 
 constexpr uint16_t ETH_TYPE_ARP = 0x0806;
 constexpr uint8_t HALEN = 6;
@@ -37,25 +40,25 @@ struct arp_frame {
     uint8_t payload[42];
 };
 
-struct arp* extract_arp(const struct ether_header* eth) {
+std::optional<struct arp> extract_arp(const struct ether_header* eth) {
     uint16_t eth_type = ntohs(eth->ether_type);
 
     if (eth_type != ETH_TYPE_ARP) {
         //printf("Type differs, not an arp packet\n");
-        return nullptr;
+        return std::nullopt;
     }
 
     const uint8_t* payload = (uint8_t*)eth + sizeof(struct ether_header);
-    struct arp* a = new struct arp;
-    a->htype = ntohs(*((const uint16_t*)payload));
-    a->ptype = ntohs(*(const uint16_t*)(payload+2));
-    a->hlen = *(payload+4);
-    a->plen = *(payload+5);
-    a->op = ntohs(*((const uint16_t*)(payload+6)));
-    memcpy(a->s_ha.data(), payload+8, HALEN);
-    memcpy(a->s_pa.data(), payload+14, PALEN);
-    memcpy(a->t_ha.data(), payload+18, HALEN);
-    memcpy(a->t_pa.data(), payload+24, PALEN);
+    struct arp a;
+    a.htype = ntohs(*((const uint16_t*)payload));
+    a.ptype = ntohs(*(const uint16_t*)(payload+2));
+    a.hlen = *(payload+4);
+    a.plen = *(payload+5);
+    a.op = ntohs(*((const uint16_t*)(payload+6)));
+    memcpy(a.s_ha.data(), payload+8, HALEN);
+    memcpy(a.s_pa.data(), payload+14, PALEN);
+    memcpy(a.t_ha.data(), payload+18, HALEN);
+    memcpy(a.t_pa.data(), payload+24, PALEN);
 
     return a;
 }
@@ -117,9 +120,14 @@ std::array<uint8_t, 42> generate_arp_frame(const haddr_t s_ha, const paddr_t s_p
     return ret;
 }
 
+struct addr_mask {
+    paddr_t addr;
+    paddr_t mask;
+};
+
 struct addr_pair {
     haddr_t haddr;
-    std::vector<paddr_t> paddrs;
+    std::vector<addr_mask> paddrs;
 };
 
 struct addr_pair get_addr_pair(const char* ifname) {
@@ -145,9 +153,12 @@ struct addr_pair get_addr_pair(const char* ifname) {
                 case AF_INET:
                     {
                         uint32_t ad = ((sockaddr_in*)sa)->sin_addr.s_addr;
+                        uint32_t nm = ((sockaddr_in*)it->ifa_netmask)->sin_addr.s_addr;
                         paddr_t pa;
                         memcpy(pa.data(), &ad, 4);
-                        ap.paddrs.push_back(pa);
+                        paddr_t mask;
+                        memcpy(mask.data(), &nm, 4);
+                        ap.paddrs.push_back({pa, mask});
                         break;
                     }
             }
@@ -171,25 +182,12 @@ std::array<char, 18> format_haddr(haddr_t ha) {
     return ret;
 }
 
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        fprintf(stderr, "%s [interface] [dst address]\n", argv[0]);
-        return -1;
-    }
-
-    const in_addr_t iaddr = inet_addr(argv[2]);
-    if (iaddr == INADDR_NONE) {
-        fprintf(stderr, "Malformed address\n");
-        return -1;
-    }
-    std::array<uint8_t, 4> dst_addr;
-    memcpy(dst_addr.data(), &iaddr, 4);
-
+int fd_open(const char* dname) {
     char device[sizeof("/dev/bpf0000")];
     int fd = -1;
     for (int i = 0; i < 10000; i++) {
         sprintf(device, "/dev/bpf%d", i);
-        fd = open(device, O_RDWR);
+        fd = open(device, O_RDWR | O_NONBLOCK);
 
         if (fd == -1 && errno == EBUSY) {
             perror("open");
@@ -204,24 +202,76 @@ int main(int argc, char** argv) {
     }
     struct ifreq ifr;
 
-    strcpy(ifr.ifr_name, argv[1]);
+    strcpy(ifr.ifr_name, dname); 
     if (ioctl(fd, BIOCSETIF, &ifr) == -1) {
         perror("ioctl");
         return -1;
     }
 
-    unsigned buf_len = 1;
-    if (ioctl(fd, BIOCIMMEDIATE, &buf_len) == -1) {
+    unsigned yes = 1;
+    if (ioctl(fd, BIOCIMMEDIATE, &yes) == -1) {
         perror("ioctl");
         return -1;
     }
 
+    return fd;
+}
+
+std::optional<std::vector<struct arp>> read_arp_resp(int fd, size_t buflen) {
+    char* buf = new char[buflen];
+    std::vector<arp> ret;
+    ssize_t len = read(fd, buf, buflen);
+    if(len <= 0) {
+        delete[] buf;
+        switch (len) {
+            case -1:
+                {
+                    if (errno == EAGAIN) {
+                        return std::move(ret);
+                    }
+                    perror("read");
+                    return std::nullopt;
+                }
+            case 0:
+                fprintf(stderr, "read: socket closed");
+                return std::nullopt;
+        }
+    }
+
+    char* packet = buf;
+    while(packet - buf < len) {
+        struct bpf_hdr* bpf_header;
+        bpf_header = (struct bpf_hdr*)packet;
+        if (bpf_header->bh_caplen >= sizeof(struct ether_header)) {
+            struct ether_header* eth;
+            eth = (struct ether_header*)(packet + bpf_header->bh_hdrlen);
+            std::optional<struct arp> a = extract_arp(eth);
+            if (a.has_value() && a->op == 2) {
+                ret.push_back(*a);
+            }
+        }
+        packet += BPF_WORDALIGN(bpf_header->bh_hdrlen + bpf_header->bh_caplen);
+    }
+    delete[] buf;
+    return std::move(ret);
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "%s [interface]\n", argv[0]);
+        return -1;
+    }
+
+    int fd;
+    if ((fd = fd_open(argv[1]) ) == -1) {
+        return -1;
+    }
+
+    unsigned buf_len;
     if (ioctl(fd, BIOCGBLEN, &buf_len) == -1) {
         perror("ioctl");
         return -1;
     }
-
-    char* buf = new char[buf_len];
 
     const struct addr_pair ap = get_addr_pair(argv[1]);
     if (ap.paddrs.empty()) {
@@ -230,35 +280,47 @@ int main(int argc, char** argv) {
     }
 
     printf("Host MAC address : %s\n", format_haddr(ap.haddr).data());
-    for(paddr_t addr : ap.paddrs) {
-        printf("Send an ARP request to %s as %s\n", format_paddr(dst_addr).data(), format_paddr(addr).data());
-        write(fd, generate_arp_frame(ap.haddr, addr, dst_addr).data(), 42);
+    for(addr_mask am : ap.paddrs) {
+        printf("[*] Sending an ARP request %s\n", format_paddr(am.addr).data());
+
+        paddr_t netaddr, bcastaddr;
+        for(int i = 0; i < PALEN; i++) {
+            netaddr[i] = am.addr[i] & am.mask[i];
+            bcastaddr[i] = am.addr[i] | (~am.mask[i]);
+        }
+
+        for(uint32_t i = htonl(*((uint32_t*)netaddr.data()))+1; i < htonl(*((uint32_t*)bcastaddr.data())); i++) {
+            uint32_t addr = ntohl(i);
+            paddr_t dst_addr;
+            memcpy(dst_addr.data(), &addr, PALEN);
+            write(fd, generate_arp_frame(ap.haddr, am.addr, dst_addr).data(), 42);
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+            auto ret = read_arp_resp(fd, buf_len);
+            if (!ret.has_value()) {
+                return -1;
+            }
+            for(arp a : *ret) {
+                if (a.t_ha == ap.haddr) {
+                    printf("%s : %s\n", format_haddr(a.s_ha).data(), format_paddr(a.s_pa).data());
+                }
+            }
+        }
     }
 
-    printf("Waiting for reply\n");
-    while(1) {
-        ssize_t len = read(fd, buf, buf_len);
-        if(len == -1) {
-            perror("read");
-            break;
+    puts("[*] ARP requests sent, waiting replies for 10 seconds...");
+    auto start = std::chrono::steady_clock::now();
+    while(std::chrono::steady_clock::now() - start < std::chrono::seconds(10)) {
+        auto ret = read_arp_resp(fd, buf_len);
+        if (!ret.has_value()) {
+            return -1;
         }
-        char* packet = buf;
-        while(packet - buf < len) {
-            struct bpf_hdr* bpf_header;
-            bpf_header = (struct bpf_hdr*)packet;
-            if (bpf_header->bh_caplen >= sizeof(struct ether_header)) {
-                struct ether_header* eth;
-                eth = (struct ether_header*)(packet + bpf_header->bh_hdrlen);
-                struct arp* a = extract_arp(eth);
-                if (a != nullptr && a->op == 2 && a->s_pa == dst_addr) {
-                    printf("%s : %s\n", format_paddr(a->s_pa).data(), format_haddr(a->s_ha).data());
-                }/* else {
-                    std::array<uint8_t, 6> haddr;
-                    memcpy(haddr.data(), eth->ether_shost, 6);
-                    printf("from %s, Not an ARP packet\n", format_haddr(haddr).data());
-                }*/
+        for(arp a : *ret) {
+            if (a.t_ha == ap.haddr) {
+                printf("%s : %s\n", format_haddr(a.s_ha).data(), format_paddr(a.s_pa).data());
+            } else {
+                printf("Arp response to the other host");
             }
-            packet += BPF_WORDALIGN(bpf_header->bh_hdrlen + bpf_header->bh_caplen);
         }
     }
 
